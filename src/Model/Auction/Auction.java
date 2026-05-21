eb29d890047bb4e515a8a6f6d6e76cf2d1a36827
Phuc2511-uet <Inngu2007@gmail.com>
2026-05-21 13:00:46 +0700

    sua lai loi logic timer

--- src/Model/Auction/Auction.java
+++ src/Model/Auction/Auction.java
@@ -259,7 +259,30 @@ public class Auction {
         DatabaseManager.saveOrUpdateAuction(this);
     }
     public void resumeAfterRestart() {
-        scheduleFinish();
+
+        long now = System.currentTimeMillis();
+
+        // ✅ Nếu đã hết hạn
+        if (endTime <= now && currentStatus == Status.RUNNING) {
+
+            lock.lock();
+            try {
+                transitionTo(Status.FINISH);
+                DatabaseManager.saveOrUpdateAuction(this);
+            } finally {
+                lock.unlock();
+            }
+
+            notifyObservers("STATUS_CHANGED " + id + " FINISH");
+            notifyObservers("AUCTION_FINISHED " + id);
+
+            return;
+        }
+
+        // ✅ Nếu chưa hết hạn → schedule lại
+        if (currentStatus == Status.RUNNING) {
+            scheduleFinish();
+        }
     }
     public void forceFinish() {
 
@@ -351,6 +374,13 @@ public class Auction {
                 if (newPrice - currentPrice < MIN_INCREMENT) {
                     throw new InvalidBidException("Bước_giá_tối_thiểu_là_100");
                 }
+                // nhả tiền người cũ
+                if (currentBidder != null) {
+                    currentBidder.release(currentPrice);
+                }
+
+                // giữ tiền người mới
+                bidder.reserve(newPrice);
 
                 currentPrice = newPrice;
                 currentBidder = bidder;
